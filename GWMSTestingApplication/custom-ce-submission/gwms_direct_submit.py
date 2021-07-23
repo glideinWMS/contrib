47b02b2f44eaf50f65b05fa3eaed2df10595dc23
Maia Boyd <mboyd6@fnal.gov>
2021-07-23 13:13:10 -0500

    updated arguments in gwms_direct_submit.py

--- GWMSTestingApplication/custom-ce-submission/gwms_direct_submit.py
+++ GWMSTestingApplication/custom-ce-submission/gwms_direct_submit.py
@@ -9,38 +9,31 @@ import shutil
 
 from generate_submit_environment import generate_submit_environment
 
-#Find Absolute and Working Directory Paths 
 
-#job.env
 WORK_DIR = os.getcwd()
 print(f"User working directory: {WORK_DIR}")
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 print(f"Script root directory: {ROOT_DIR}")
 
-# /var/lib/gwms-factory/work-dir/entry_fermicloud528/job.condor
-# /var/lib/gwms-factory/work-dir/entry_fermicloud489/job.condor
+
 def make_condor(args):
-    jobconFile = "/var/lib/gwms-factory/work-dir/entry_" + str(args.GLIDEIN_ENTRY_NAME) + "/job.condor"
+    jobconFile = "/var/lib/gwms-factory/work-dir/entry_" + str(args.ENTRY_NAME) + "/job.condor"
     sub_list = [
         (r"\bLog\s=\s.*", "Log = $ENV(LOGFILE)"),
         (r"\bOutput\s=\s.*", "Output = $ENV(OUTPUTFILE)"),
         (r"\bError\s=\s.*", "Error = $ENV(ERRORFILE)"),
-        (r"\bExecutable\s=\s.*", "Executable = $ENV(GLIDEIN_EXECUT_TEST)")
+        (r"\bExecutable\s=\s.*", "Executable = $ENV(EXECUTABLE)")
     ]
-    tf = tempfile.NamedTemporaryFile("w", delete=False)
-    file = open(jobconFile, "r").read()
+    tf = tempfile.NamedTemporaryFile("w", delete=False) 
+    file = open(jobconFile, "r").read() #make a temporary copy of the condor file
     
-    for s in sub_list:
+    for s in sub_list: #change the temporary file variables
         file = re.sub(s[0], s[1], file)
-    tf.writelines(file)
+    tf.writelines(file) #write a new temporary file
     tf.close()
 
-    # Make a temporary copy of the condor file
-    # Change the temporary file variables to fit out needs
-    # Write the new temporary file
     return tf.name # return the temporary file path
-# jobconFile = ROOT_DIR + "/job.condor"
 
 def submit(jobenvFile, jobconFile):
     # Call condor_submit
@@ -72,9 +65,9 @@ def make_environment(args):
 
     #executable
 
-    if not os.path.isabs(args.GLIDEIN_EXECUT_TEST):
-        args.GLIDEIN_EXECUT_TEST = WORK_DIR + "/" + args.GLIDEIN_EXECUT_TEST
-    execFile = args.GLIDEIN_EXECUT_TEST
+    if not os.path.isabs(args.ERRORFILE):
+        args.EXECUTABLE = WORK_DIR + "/" + args.EXECUTABLE
+    execFile = args.EXECUTABLE
 
 
     # Do some processing
@@ -86,20 +79,21 @@ def make_environment(args):
     env["LOGFILE"] = logFile
     env["OUTPUTFILE"] = outFile
     env["ERRORFILE"] = errorFile
-    env["GLIDEIN_EXECUT_TEST"] = execFile
+    env["EXECUTABLE"] = execFile
+    env["ARGUMENTS"] = str(args.ARGUMENT)
 
     return env
 
 if __name__ == "__main__":
     # Parse arguments from the terminal
     parser = argparse.ArgumentParser(description="Process this condor job")
-    parser.add_argument('GLIDEIN_COUNT', type=int)   
-    # parser.add_argument('--entry-name', dest='GLIDEIN_ENTRY_NAME', action='store', const=str, nargs='?')
-    parser.add_argument('GLIDEIN_ENTRY_NAME')
-    parser.add_argument('GLIDEIN_EXECUT_TEST')
-    parser.add_argument('LOGFILE')
-    parser.add_argument('OUTPUTFILE')
-    parser.add_argument('ERRORFILE')
+    parser.add_argument('--entry', dest='ENTRY_NAME')
+    parser.add_argument('--logfile', dest='LOGFILE')
+    parser.add_argument('--outfile', dest='OUTPUTFILE')
+    parser.add_argument('--errfile', dest='ERRORFILE')
+    parser.add_argument('-n', dest='GLIDEIN_COUNT', type=int)
+    parser.add_argument('EXECUTABLE')
+    parser.add_argument('ARGUMENT', type=str, nargs='*')
     args = parser.parse_args()
 
     env = make_environment(args)
